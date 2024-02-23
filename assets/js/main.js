2f06793b4a93cab0684ec31916594450c4710103
ErlPrtm <teukuerlanggapratama@gmail.com>
2024-02-23 20:59:28 +0700

    Berhasil send message ke Email

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,34 @@ const shadowHeader = () =>{
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
     this.scrollY >= 50 ? header.classList.add('shadow-header') 
-                       : header.classList.remove('shadow-header')
+    : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
+
+// ============ EMAIL JS ============ //
+const contactForm = document.getElementById('contact__form'),
+        contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form -publicKey
+    emailjs.sendForm('service_d3xwfpi', 'template_5ucu89p', '#contact__form', 'ahpRFX9eBOUNZQk3T')
+    .then(() =>{
+        // Show sent message 
+        contactMessage.textContent = 'Message sent succesfully ✅'
+
+        // Remove message after five seconds 
+        setTimeout(() => {
+           contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset();
+    }, () => {
+        // Show error message
+        contactMessage.textContent = 'Message not sent (Services Error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
